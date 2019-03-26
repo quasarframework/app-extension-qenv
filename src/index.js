b2f792504c458d98efbe470a52a4b7a111468be1
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-03-26 09:37:20 -0600

    Initial code

---
+++ src/index.js
@@ -0,0 +1,85 @@
+/**
+ * Quasar App Extension index/runner script
+ * (runs on each dev/build)
+ *
+ * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/IndexAPI.js
+ */
+
+const fs = require('fs')
+
+const extendConfig = function (api, conf) {
+  // see if QENV is set
+  if (!process.env.QENV) {
+    console.error(`! App Extension (qenv): missing QENV environment variable; skipping`)    
+    return
+  }
+
+  const qEnvName = process.env.QENV
+
+  if (qEnvName === void 0 || qEnvName === '') {
+    console.error(`! App Extension (qenv): missing QENV environment variable; skipping`)    
+    return
+  }
+
+  // split names into array
+  const names = qEnvName.split('+')
+
+  let envName = '.quasar.env.json'
+
+  // resolve the path to the file
+  const envPath = api.resolve.app(envName)
+
+  // check file exists
+  if (!fs.existsSync(envPath)) {
+    console.error(`! App Extension (qenv): '${envPath}' file missing; skipping`)
+    return
+  }
+
+  let envData
+  try {
+    envData = require(envPath)
+  }
+  catch(e) {
+    console.error(`! App Extension (qenv): Error '${JSON.stringify(e)}'`)
+    return
+  }
+
+  let data = {}
+
+  names.forEach((name) => {
+    if (name in envData) {
+      data = Object.assign(data, envData[name])
+    }
+    else {
+      console.error(`! App Extension (qenv): Missing '${name}' from ${envName}; skipping`)
+    }
+  })
+  
+  // make sure there is a build.env object
+  if (!conf.build.env) {
+    conf.build.env = {}
+  }
+
+  // for brevity
+  let target = conf.build.env
+
+  // check for common root object
+  if (api.prompts.common_root_object && api.prompts.common_root_object !== 'none') {
+    let rootObject = api.prompts.common_root_object
+
+    if (!target[rootObject]) {
+      target[rootObject] = {}
+      target = target[rootObject]
+    }
+  }
+
+  for (const key in data) {
+    target[key] = JSON.stringify(data[key])
+  }
+}
+
+module.exports = function (api, ctx) {
+  api.extendQuasarConf((conf) => {
+    extendConfig(api, conf)
+  })
+}
---
+++ src/install.js
@@ -0,0 +1,31 @@
+/**
+ * Quasar App Extension install script
+ *
+ * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/InstallAPI.js
+ */
+const fs = require('fs')
+
+module.exports = function (api) {
+  api.render('./templates', {})
+
+  const envName = '.quasar.env.json'
+
+  // does user want .quasar.env.json added to .gitignore?
+  if (api.prompts.add_qenv_to_gitignore === true) {
+    const gitignorePath = api.resolve.app('.gitignore')
+    // read .gitignore
+    let buffer = fs.readFileSync(gitignorePath, 'utf8')
+    // convert to array
+    let data = buffer.split('\n')
+    // See if the .quasar.env.json file already exists in .gitignore
+    if (!data.includes(envName)) {
+      data.push(envName)
+    }
+    // rejoin array to string
+    data = data.join('\n')
+    // convert to buffer
+    buffer = Buffer.from(data)
+    // write .gitignore
+    fs.writeFileSync(gitignorePath, buffer, 'utf8')
+  }
+}
