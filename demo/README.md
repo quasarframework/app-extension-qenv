e55ef86b751f1ca668d005b5852e51f95e8bacb2
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-04-16 14:14:06 -0600

    chore: add demo to repository

---
+++ demo/README.md
@@ -0,0 +1,9 @@
+# QEnv: Quasar App Extension Demp app
+
+From the terminal, you can `yarn` any of the commands:
+
+    "dev": "QENV=development quasar dev",
+    "test-dev": "QENV=test quasar dev",
+    "combined": "QENV=development+test quasar dev",
+    "build": "QENV=production quasar build",
+    "blah": "QENV=blah quasar build",
---
+++ demo/src/pages/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="col-lg-3 col-sm-6 col-sm-10 col-xs-12">
+      <div>Open the browser Dev Tools, then find "QEnv Test" and expland it</div>
+      <div>You should find something similar to (depending on which QEnv was run):</div>
+      <div>
+        <pre>
+jeff:
+  ENV_DEV: "Development"
+  ENV_TYPE: "Running Development"
+  MY_PORT: 4000
+        </pre>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+
+  mounted () {
+    console.log('QEnv Test', process.env)
+  }
+}
+</script>
