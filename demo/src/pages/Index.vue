9bb87e934ed0ab9a8a83f5606ebcc3b532ef0709
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-06-19 07:42:23 -0600

    chore(demo): adjust main page

--- demo/src/pages/Index.vue
+++ demo/src/pages/Index.vue
@@ -1,14 +1,16 @@
 <template>
   <q-page class="flex flex-center">
     <div class="col-lg-3 col-sm-6 col-sm-10 col-xs-12">
-      <div>Open the browser Dev Tools, then find "QEnv Test" and expand it</div>
-      <div>You should find something similar to (depending on which QEnv was run):</div>
+      <div>Open the browser Dev Tools</div>
+      <div>Using common root object "test"</div>
+      <div>You should find something similar to (depending on which QEnv environment was run):</div>
       <div>
         <pre>
-jeff:
-  ENV_DEV: "Development"
-  ENV_TYPE: "Running Development"
-  MY_PORT: 4000
+test: {
+  "ENV_TYPE": "Running Development",
+  "ENV_DEV": "Development",
+  "MY_PORT": 4000
+}
         </pre>
       </div>
     </div>
@@ -24,7 +26,7 @@ export default {
 
   mounted () {
     /* eslint-disable no-console */
-    console.log('QEnv Test', process.env)
+    console.log('test:', JSON.stringify(process.env.test, null, 2))
   }
 }
 </script>
